7b8f0afd9d779f1da256231e2aa92b6c4128e0c2
aloima <acar.aloima@gmail.com>
2025-12-20 23:47:23 +0300

    feat(benchmark): add time measure and time type

--- benchmark.go
+++ benchmark.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"strings"
+	"time"
 )
 
 func PrintBenchmarkTypes() {
@@ -30,24 +31,33 @@ func create_client(id int, host string, port int) net.Conn {
 	return conn
 }
 
-func benchmark(client net.Conn, in string) {
+func benchmark(completed_req_count *uint64, req_count uint64, client net.Conn, in string) {
 	buf := make([]byte, 1024)
-	fmt.Fprint(client, in)
 
 	for {
-		n, err := client.Read(buf)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if n != 1024 {
+		if *completed_req_count >= req_count {
 			break
 		}
+
+		fmt.Fprint(client, in)
+
+		for {
+			n, err := client.Read(buf)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if n != 1024 {
+				break
+			}
+		}
+
+		*completed_req_count += 1
 	}
 }
 
-func StartBenchmark(host string, port int, types string, client_count int, request_count uint64) {
+func StartBenchmark(host string, port int, types string, client_count int, req_count uint64) {
 	var clients []net.Conn
 
 	for i := range client_count {
@@ -67,14 +77,30 @@ func StartBenchmark(host string, port int, types string, client_count int, reque
 		case "ping":
 			in = "*1\r\n$4\r\nPING\r\n"
 
+		case "time":
+			in = "*1\r\n$4\r\nTIME\r\n"
+
 		default:
 			log.Fatalf("invalid type: %s\n", t)
 		}
 
+		completed_req_count := uint64(0)
+
 		for i := range client_count {
-			go benchmark(clients[i], in)
+			go benchmark(&completed_req_count, req_count, clients[i], in)
 		}
 
-		// TODO: add waiter for clients
+		start := time.Now()
+
+		for {
+			if completed_req_count >= req_count {
+				elapsed := time.Since(start)
+				fmt.Printf("====== %s ======\n", strings.ToUpper(t))
+				fmt.Printf("%d parallel clients\n", client_count)
+				fmt.Printf("%d requests completed in %.3f seconds\n", completed_req_count, elapsed.Seconds())
+				fmt.Printf("%.3f requests per second\n\n", float64(completed_req_count) / elapsed.Seconds())
+				break
+			}
+		}
 	}
 }
